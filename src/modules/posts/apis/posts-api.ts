e6fff042dd7ca86a23c9d966269287054125ad03
emremutlu08 <mutluemre93@gmail.com>
2023-06-26 23:01:05 +0300

    feat(api): created frontend api

---
+++ src/modules/posts/apis/posts-api.ts
@@ -0,0 +1,43 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+import { Post, Posts } from "../types/posts-type";
+
+export const postsApi = createApi({
+  reducerPath: "postApi",
+  refetchOnFocus: true,
+  baseQuery: fetchBaseQuery({
+    baseUrl: `${process.env.APP_HOST}/api/posts`, // TODO: replace with your own API
+  }),
+  endpoints: (builder) => ({
+    createPost: builder.mutation<Post, Partial<Post>>({
+      query: (body) => ({
+        url: "",
+        method: "POST",
+        body: JSON.stringify({ ...body }),
+      }),
+    }),
+    readPosts: builder.query<Posts, { limit?: number; skip?: number }>({
+      query: ({ limit = 3, skip = 3 }) => `?limit=${limit}&skip=${skip}`,
+    }),
+    updatePost: builder.mutation<Post, Partial<Post>>({
+      query: ({ id, ...body }) => ({
+        url: `/${id}`,
+        method: "PUT",
+        body: JSON.stringify({ ...body }),
+      }),
+    }),
+    deletePost: builder.mutation<Post, Partial<Post>>({
+      query: ({ id }) => ({
+        url: `/${id}`,
+        method: "DELETE",
+      }),
+    }),
+  }),
+});
+
+export const {
+  useCreatePostMutation,
+  useReadPostsQuery,
+  useUpdatePostMutation,
+  useDeletePostMutation,
+} = postsApi;
---
+++ src/modules/posts/apis/types/type.ts
@@ -0,0 +1,3 @@
+export type PostsParams = {
+  params: { id: string };
+};

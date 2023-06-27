441144433fb5a2ce1063da5c66c38d37976be777
emremutlu08 <mutluemre93@gmail.com>
2023-06-28 00:35:39 +0300

    feat(posts): added post-create-form to the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import PostCardsInfiniteScroll from "@/modules/posts/components/post-cards-infinite-scroll";
+import PostCreateForm from "@/modules/posts/components/post-create-form";
 
 export default function Home() {
   return (
     <main>
+      <PostCreateForm />
       <PostCardsInfiniteScroll />
     </main>
   );

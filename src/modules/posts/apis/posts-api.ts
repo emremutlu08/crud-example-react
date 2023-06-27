16b8079c76704b0ec2b81b503f96416a6539c21a
emremutlu08 <mutluemre93@gmail.com>
2023-06-27 21:27:42 +0300

    feat(posts): created toasts on responses

--- src/modules/posts/apis/posts-api.ts
+++ src/modules/posts/apis/posts-api.ts
@@ -1,4 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { toast } from "react-toastify";
 
 import { Post, Posts } from "../types/posts-type";
 
@@ -13,22 +14,66 @@ export const postsApi = createApi({
         url: "",
         method: "POST",
         body: JSON.stringify({ ...body }),
+        responseHandler(response) {
+          if (!response.ok) {
+            toast("Couldn't created!", { type: "error" });
+          } else {
+            toast("Successfully created!");
+          }
+
+          return response.json().then((data) => {
+            return { ...data };
+          });
+        },
       }),
     }),
     readPosts: builder.query<Posts, { limit?: number; skip?: number }>({
-      query: ({ limit = 3, skip = 3 }) => `?limit=${limit}&skip=${skip}`,
+      query: ({ limit = 3, skip = 3 }) => ({
+        url: `?limit=${limit}&skip=${skip}`,
+        responseHandler(response) {
+          if (!response.ok) {
+            toast("Couldn't read!", { type: "error" });
+          }
+
+          return response.json().then((data) => {
+            return { ...data };
+          });
+        },
+      }),
     }),
     updatePost: builder.mutation<Post, Partial<Post>>({
       query: ({ id, ...body }) => ({
         url: `/${id}`,
         method: "PUT",
         body: JSON.stringify({ ...body }),
+        responseHandler(response) {
+          if (!response.ok) {
+            toast("Couldn't updated!", { type: "error" });
+          } else {
+            toast("Successfully updated!");
+          }
+
+          return response.json().then((data) => {
+            return { ...data, id };
+          });
+        },
       }),
     }),
     deletePost: builder.mutation<Post, Partial<Post>>({
       query: ({ id }) => ({
         url: `/${id}`,
         method: "DELETE",
+        responseHandler(response) {
+          if (!response.ok) {
+            toast("Couldn't deleted!", { type: "error" });
+          } else {
+            toast("Successfully deleted!");
+          }
+
+          return response.json().then((data) => {
+            return { ...data, id };
+          });
+        },
       }),
     }),
   }),

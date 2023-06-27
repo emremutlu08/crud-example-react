ba268265f680d863342e22f9db2e906685c9658f
emremutlu08 <mutluemre93@gmail.com>
2023-06-27 18:47:09 +0300

    feat(posts): created post cards  infinite scroll component

---
+++ src/modules/posts/components/post-cards-infinite-scroll.tsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+
+import InfiniteScroll from "react-infinite-scroll-component";
+
+import { useAppDispatch, useAppSelector } from "@/modules/redux/hooks";
+
+import PostCard from "./post-card";
+import { useReadPostsQuery } from "../apis/posts-api";
+import { addPosts } from "../slices/posts-slice";
+
+export default function PostCardsInfiniteScroll() {
+  const dispatch = useAppDispatch();
+  const posts = useAppSelector((state) => state.posts.posts);
+
+  const [skip, setSkip] = useState(0);
+  const { isFetching, data } = useReadPostsQuery({
+    skip,
+  });
+
+  // Next Page Function
+  const handleNextPage = () => {
+    setSkip((prev) => prev + 3);
+  };
+
+  useEffect(() => {
+    if (!isFetching && data) {
+      dispatch(addPosts({ ...data }));
+    }
+  }, [data, isFetching, dispatch]);
+
+  return (
+    <InfiniteScroll
+      dataLength={posts.length}
+      next={handleNextPage}
+      hasMore={data?.total !== posts.length}
+      loader={<h4>Loading...</h4>}
+      endMessage={
+        <p style={{ textAlign: "center" }}>
+          <b>Yay! You have seen it all</b>
+        </p>
+      }
+      className="flex flex-wrap gap-4 justify-center pt-10 px-2"
+    >
+      {posts.map((item) => (
+        <PostCard item={item} key={"post-" + item.id} />
+      ))}
+    </InfiniteScroll>
+  );
+}
